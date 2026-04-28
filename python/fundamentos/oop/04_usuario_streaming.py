3d376dc017af33245e10d41c77100d320a639a4d
Jheimy Tolentino <araelanabalon@liceovvh.cl>
2026-04-28 12:57:56 -0400

    Update 04_usuario_streaming.py

--- python/fundamentos/oop/04_usuario_streaming.py
+++ python/fundamentos/oop/04_usuario_streaming.py
@@ -5,36 +5,69 @@ class UsuarioStreaming:
         self.suscripcion = suscripcion
         self.lista_reproduccion = []
 
-
     def agregar_a_lista(self, titulo):
         """Agrega un contenido a la lista de reproducción del usuario."""
         self.lista_reproduccion.append(titulo)
-        print(usuario1.lista_reproduccion)
+        print(f'El video "{titulo}" fué agregado correctamente.')
+
 
     def ver_contenido(self, titulo):
         """Simula que el usuario reproduce un contenido."""
+        if titulo in self.lista_reproduccion:
+            print(f"Usuario {self.nombre} está viendo: {titulo}")
+        else:
+            print(f'Título "{titulo}" no encontrado . . .')
 
-        pass
 
     def cambiar_suscripcion(self, nueva_suscripcion):
         """Cambia el tipo de suscripción del usuario."""
-        pass
+        
 
     def mostrar_info_usuario(self):
         """Muestra la información del usuario y su lista de reproducción."""
         pass
 
-usuario1 = UsuarioStreaming("Ara", "araelanabalon@liceovvh.cl")
+def crearUsuario():
+    nombre = input("Ingresa el nombre de usuario: ")
+    email = input("Ingresa una dirección de gmail: ")
+    while '@gmail.com' not in email:
+        print("Asegurate de usar una cuenta de Google")
+        email = input("Ingresa una dirección de GMAIL: ")
+    return UsuarioStreaming(nombre, email)
+usuario = crearUsuario()
 
-reproducciones = []
-def agregarRepro():
-    cantidad = int(input("Cuantos títulos desea agregar?: "))
-    for i in range(cantidad):
-        nuevaReproduccion = input("Ingrese nueva reproducción: ")
-        reproducciones.append(nuevaReproduccion)
-    usuario1.agregar_a_lista(reproducciones)
-agregarRepro()
-def verRepro():
+def anadirVideo():
+    repro = input("Ingresa el nombre del video: ")
+    usuario.agregar_a_lista(repro)
+    return repro
 
-    pass
+continuar = True
+while continuar:
+    print("\n---Ejercicios Python ---")
+    print("--- 1. Agragar a lista ---")
+    print("--- 2. Ver video---")
+    print("--- 3. Cambiar suscripción ---")
+    print("--- 4. Mostrar info ---")
+    opcion = input("\n---Elije una opción: (1-4) (0 para salir)")
+    if opcion == "1":
+        print("\nEjecutando 1era instrucción: ")
+        anadirVideo()
+    elif opcion == "2":
+        print("\nEjecutando 2da instrucción: ")
+        usuario.ver_contenido()
+    elif opcion == "3":
+        print("\nEjecutando 3era instrucción: ")
+        
+    elif opcion == "4":
+        print("\nEjecutando 4ta instrucción: ")
+        
+    elif opcion == "0":
+        print("Saliendo...")
+        continuar = False
+    else:
+        print("Opción no válido. Intenta otra vez.")
+
+
+
+
 
